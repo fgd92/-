388167ed6b52299863802592d3dd77f54e343ded
fgd <vinockurow.iwan2015@yande.ru>
2020-09-22 19:33:47 +0400

    added player move, added press E for Open door

---
+++ Assets/Scripts/InteractWithObject.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractWithObject : MonoBehaviour
+{
+    float distance = 10f;
+
+    void Update()
+    {
+        RaycastHit hit;
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Vector3 forward = transform.TransformDirection(Vector3.forward) * distance;
+            Ray ray = new Ray(transform.position, forward);
+
+            Physics.Raycast(ray, out hit, distance);
+
+            if (hit.collider != null)
+            {
+                if (hit.collider.gameObject.tag == "Door")
+                {
+                    GameObject door = hit.collider.gameObject;
+                    Animator animator = door.GetComponent<Animator>();
+                    animator.SetBool("IsOpened", !animator.GetBool("IsOpened"));
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ThirdPersonMovement.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonMovement : MonoBehaviour
+{
+    public CharacterController Controller;
+    public Transform CameraTransform;
+
+    public float Speed = 10f;
+    public float TurnSmoothTime = 0.1f;
+    public float JumpHeight;
+    public float Gravity;
+    float _turnSmoothVelocity;
+    Vector3 _velocity;
+    public Transform GroundCheck;
+    public float GroundDistance = 0.4f;
+    public LayerMask GroundMask;
+    bool _isGrounded;
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        _isGrounded = Physics.CheckSphere(GroundCheck.position, GroundDistance, GroundMask);
+
+        if (_isGrounded && _velocity.y < 0)
+        {
+            _velocity.y = -2f;
+        }
+
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0f, vertical).normalized;
+
+        if (direction.magnitude >= 0.1f)
+        {
+            float targetAngle = Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + CameraTransform.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref _turnSmoothVelocity, TurnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            Controller.Move(moveDir.normalized * Speed * Time.deltaTime);
+        }
+
+        if(Input.GetButtonDown("Jump") && _isGrounded)
+        {
+            _velocity.y = Mathf.Sqrt(JumpHeight * -2f * Gravity);
+        }
+
+        _velocity.y += Gravity * Time.deltaTime;
+        Controller.Move(_velocity * Time.deltaTime);
+    }
+}
